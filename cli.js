2fb0e253e1b0c22970affcc922928db830fa90bc
Nicolas Luck <nicolas@lucksus.eu>
2019-08-22 15:45:38 +0200

    Check hash of downloaded/provided DNAs

---
+++ cli.js
@@ -0,0 +1,54 @@
+const { spawnSync } = require('child_process')
+
+function executablePath() {
+    let executable;
+    if (process.platform === "win32") {
+        executable = "./hc.exe"
+    } else if (process.platform === "darwin") {
+        executable = "./hc-darwin"
+    } else if (process.platform === "linux") {
+        executable = "./hc-linux"
+    } else {
+        log('error', "unsupported platform: "+process.platform)
+        return
+    }
+
+    return path.join(__dirname, executable)
+}
+
+module.exports = {
+    hash: (filePath) => {
+        console.log("CLI: hashing file", filePath)
+        let params = ["hash", "--path", filePath]
+        let { stdout, stderr, error } = spawnSync(executablePath(), params)
+        stderr = stderr.toString()
+        stdout = stdout.toString()
+        console.log("CLI: got results:")
+        console.log("stdout:", stdout)
+        console.log("stderr:", stderr)
+
+        if(error) {
+            console.log('Error executing hc:', error)
+            return {error}
+        }
+        
+        if(stderr.length > 0) {
+            error = stderr
+            console.log('Error executing hc:', error)
+            return {error}
+        }
+        
+        console.log(`hc stdout: ${JSON.stringify(stdout)}`)
+        let lines = stdout.split('\n')
+        let hash
+        for(line of lines) {
+            if(line.startsWith('DNA Hash: ')) {
+                hash = line.split("Hash: ")[1]
+            }
+            if(line.startsWith('Error:')) {
+                error = line
+            }
+        }
+        return { hash, error }
+    }
+}
