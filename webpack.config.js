71c30f1e28dde0e9b4411e4fa4f6ae376b73d473
Nicolas Luck <nicolas@lucksus.eu>
2019-12-08 22:51:01 +0100

    Build debug_view.js with webpack to make Vuetify work

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+
+module.exports = {
+  entry: './views/debug_view.js',
+  output: {
+    filename: 'debug_view.dist.js',
+    path: path.resolve(__dirname, 'views'),
+  },
+  target: "electron-renderer",
+  module: {
+    rules: [
+        {
+          test: /\.s(c|a)ss$/,
+          use: [
+            'vue-style-loader',
+            'css-loader',
+            {
+              loader: 'sass-loader',
+              // Requires sass-loader@^7.0.0
+              options: {
+                implementation: require('sass'),
+                fiber: require('fibers'),
+                indentedSyntax: true // optional
+              },
+              // Requires sass-loader@^8.0.0
+              options: {
+                implementation: require('sass'),
+                sassOptions: {
+                  fiber: require('fibers'),
+                  indentedSyntax: true // optional
+                },
+              },
+            },
+          ],
+        },
+        {
+            test: /\.vue$/,
+            loader: 'vue-loader'
+          }
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new VuetifyLoaderPlugin()
+  ],
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+};
